f542b98fc43db4529fb18311acbed790ee236f93
sailboat-anon <sailboat-anon@protonmail.com>
2022-01-09 11:31:11 -0800

    .

--- wardriver/projects/wardriver/src/module.py
+++ wardriver/projects/wardriver/src/module.py
@@ -12,9 +12,10 @@ scan_pid = None
 @module.handles_action('basic_wardriver_flow')
 def basic_wardriver_flow(request: Request):
     global scan_pid
-    proc = subprocess.Popen(['/usr/bin/python', '/tmp/m.py >> /tmp/out.log'])  # https://code.luasoftware.com/tutorials/python/simple-guide-to-subprocess/
-    scan_pid = proc.id;
-    return True
+    with open("wd-out.log","wb") as out, open("wd-error.log","wb") as err:
+        proc = subprocess.Popen(['/usr/bin/python', '/tmp/m.py >> /tmp/out.log'], stdout=out, stderr=err)  # https://code.luasoftware.com/tutorials/python/simple-guide-to-subprocess/
+        scan_pid = proc.pid
+        return True
 
 if __name__ == '__main__':
     module.start()
