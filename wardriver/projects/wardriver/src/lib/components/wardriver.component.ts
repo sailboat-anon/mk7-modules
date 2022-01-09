338418bbb86c828af0929f483ad01edfb7676d23
sailboat-anon <sailboat-anon@protonmail.com>
2022-01-09 12:34:35 -0800

    .

--- wardriver/projects/wardriver/src/lib/components/wardriver.component.ts
+++ wardriver/projects/wardriver/src/lib/components/wardriver.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'; 
 import { ApiService } from '../services/api.service'; 
 import {MatTabsModule} from '@angular/material/tabs'; 
+import fs from 'fs';
+import { Subscription, Observable, timer } from 'rxjs';
 
 @Component({ 
     selector: 'lib-wardriver', 
@@ -10,6 +12,9 @@ import {MatTabsModule} from '@angular/material/tabs';
 
 export class WarDriverComponent implements OnInit { 
     constructor(private API: ApiService) { }
+    statusWindowMsg = "Berserker module locked and loaded!";
+    subscription: Subscription;
+    everyFiveSeconds: Observable<number> = timer(0, 5000);
 
     basic_wardriver_flow(): void {
         this.API.request({
@@ -19,8 +24,31 @@ export class WarDriverComponent implements OnInit {
             
         })
     }
+
+    get_berserker_scan_status(): any {
+        let outlog = '/tmp/wd-out.log';
+        let errlog = '/tmp/wd-error.log';
+
+        if (fs.existsSync(errlog)) {
+            fs.readFile(errlog, 'utf8', (err, data) => {
+                this.statusWindowMsg = data;
+                console.log(data);
+            });
+        } 
+        if (fs.existsSync(outlog)) {
+            fs.readFile(errlog, 'utf8', (err, data) => {
+                this.statusWindowMsg = data;
+                console.log(data);
+            });
+        }
+    }
     
     ngOnInit() { 
-
+        this.subscription = this.everyFiveSeconds.subscribe(() => {
+            this.get_berserker_scan_status();
+        });
+    }
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 }
--- wardriver/projects/wardriver/src/module.py
+++ wardriver/projects/wardriver/src/module.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3 
 #todo: filter for 'Open' networks
-import logging, subprocess
+import logging, subprocess, os
 from pineapple.modules import Module, Request
 
 module = Module('wardriver', logging.DEBUG)
 
 scan_pid = None
+out_file = "/tmp/wd-out.log"
+error_file = "/tmp/wd-error.log"
 
 @module.handles_action('poll_berserker_process')
 def poll_berserker_process(request: Request):
@@ -19,7 +21,14 @@ def poll_berserker_process(request: Request):
 @module.handles_action('basic_wardriver_flow')
 def basic_wardriver_flow(request: Request):
     global scan_pid
-    with open("/tmp/wd-out.log","wb") as out, open("/tmp/wd-error.log","wb") as err:
+    global out_file
+    global error_file
+
+    if os.path.exists(out_file):
+        os.remove(out_file)
+    if os.path.exists(error_file):
+        os.remove(error_file)
+    with open(out_file,"wb") as out, open(error_file,"wb") as err:
         proc = subprocess.Popen(['/usr/bin/python', '/tmp/m.py'], stdout=out, stderr=err)  # https://code.luasoftware.com/tutorials/python/simple-guide-to-subprocess/
         scan_pid = proc.pid
         return True
