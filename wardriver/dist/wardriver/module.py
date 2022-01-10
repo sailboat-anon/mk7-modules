443b192f1af04be4adbecfa780388a61cfeaa48b
sailboat-anon <sailboatanon@protonmail.com>
2022-01-10 14:51:37 -0800

    .

--- wardriver/dist/wardriver/module.py
+++ wardriver/dist/wardriver/module.py
@@ -17,7 +17,7 @@ berserker_file = "/tmp/m.py"
 def scan_toggle_checked(request: Request):
     global scan_toggle
     global berserker_file
-    berserkerRunning = cmd.grep_output('ps -aux', berserker_file) 
+    berserkerRunning = cmd.grep_output('ps -aux', berserker_file, '| grep -v grep') 
     if len(berserkerRunning) > 1:
         return True 
     else:
@@ -28,7 +28,7 @@ def get_berserker_scan_status(request: Request):
     global out_file
     global berserker_file
     global scan_toggle
-    berserkerRunning = cmd.grep_output('ps -aux', berserker_file) 
+    berserkerRunning = cmd.grep_output('ps -aux', berserker_file, '| grep -v grep') 
     if len(berserkerRunning) > 1:
         f = open(out_file,"r")
         statusWindowOut = f.readlines()
